475946a033fe987ccaa81231a8db842b2cd58189
NaNa923 <k5279kr@gmail.com>
2024-11-15 16:01:45 +0900

    aos(스크롤애니메이션)라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,37 @@ import Account from "@/layout/Account/Account";
 import Location from "@/layout/Location/Location";
 import ShareButtons from "@/layout/Share/ShareButtons";
 import { NavermapsProvider } from "react-naver-maps";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <NavermapsProvider ncpClientId="wkb2ll76q4">
       <Container>
         <Wrapper>
           <Main />
         </Wrapper>
-        <Wrapper>
+        <Wrapper data-aos="fade-up">
           <Invitation />
         </Wrapper>
-        <Wrapper>
+        <Wrapper data-aos="fade-up">
           <Gallery />
         </Wrapper>
-        <Wrapper>
+        <Wrapper data-aos="fade-up">
           <Account />
         </Wrapper>
-        <Wrapper>
+        <Wrapper data-aos="fade-up">
           <Location />
         </Wrapper>
-        <Wrapper>
+        <Wrapper data-aos="fade-up">
           <ShareButtons />
         </Wrapper>
       </Container>
